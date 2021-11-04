76ac5ef78629d9a17dccfed37059d8ddecfc8c28
David Gloriod <dgloriod@c2r2s4.42lausanne.ch>
2021-11-04 15:03:24 +0100

    Before push

--- libft/libft-test.sh
+++ libft/libft-test.sh
@@ -5,11 +5,6 @@ cd libft-unit-test
 make fclean && make && ./run_test
 cd ..
 cd libft
-git clone https://github.com/ska42/libft-war-machine.git
-cd libft-war-machine
-bash grademe.sh
-bash grademe.sh
-cd ..
 git clone https://github.com/tripouille/libftTester
 cd libftTester
 make m
--- libft/libft/ft_strlcat.c
+++ libft/libft/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: dgloriod <dgloriod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 14:37:22 by dgloriod          #+#    #+#             */
-/*   Updated: 2021/11/03 18:36:04 by dgloriod         ###   ########.fr       */
+/*   Updated: 2021/11/04 15:00:23 by dgloriod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	len;
 	size_t	i;
@@ -20,24 +20,22 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 	len = ft_strlen(dst);
 	i = 0;
 	if (size == 0)
-		return ft_strlen(src);
+		return (ft_strlen(src));
 	if (size - len - 1 > size)
-	{
-		while ((long long) i < (long long) (size - len - 1) && src[i]) {
+		while ((long long) i < (long long)(size - len - 1) && src[i])
+		{
 			dst[len + i] = src[i];
 			i++;
 		}
-	}
 	else
-	{
-		while (i < size - len - 1 && src[i]) {
+		while (i < size - len - 1 && src[i])
+		{
 			dst[len + i] = src[i];
 			i++;
 		}
-	}
 	if (i < size || len <= size)
 		dst[len + i] = '\0';
 	if (size < len)
-		return ft_strlen(src) + size;
-	return ft_strlen(src) + len;
+		return (ft_strlen(src) + size);
+	return (ft_strlen(src) + len);
 }
