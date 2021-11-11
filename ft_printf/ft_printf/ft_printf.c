700102f52c0acfff41134501c455819befcf4f35
David Gloriod <dgloriod@c2r2s4.42lausanne.ch>
2021-11-11 15:15:42 +0100

    Save

--- ft_printf/ft_printf/ft_printf.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int ft_printf(const char *s, ...)
-{
-  va_list args;
-  printf("%s\n", args);
-}
--- ft_printf/ft_printf/main.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-
-int main()
-{
-  ft_printf("Hello %s", "Hello", "123", "1234");
-  return (0);
-}
